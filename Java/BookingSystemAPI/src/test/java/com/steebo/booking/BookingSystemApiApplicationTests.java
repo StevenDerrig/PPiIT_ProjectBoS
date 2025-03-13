a3e18e3236f5193cf1a9208a3e3007cc96155068
Steebo <G00424644@atu.ie>
2025-03-13 11:15:49 +0000

    JavaREST back end starting structure

---
+++ Java/BookingSystemAPI/src/test/java/com/steebo/booking/BookingSystemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steebo.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingSystemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
