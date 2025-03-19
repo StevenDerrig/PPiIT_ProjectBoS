65a8607f1d6376d7af364d429ae1e0f98d51bdca
Steebo <G00424644@atu.ie>
2025-03-19 20:35:01 +0000

    Adding Net Maui frontend, starting code

---
+++ NetMaui/BookingApp/BookingApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace BookingApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
